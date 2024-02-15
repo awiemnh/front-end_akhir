5a98c9d4aa35e81f7d65a3c188bbdbb9841da02e
Muhammad Nur Habibi <awie.mnh@gmail.com>
2024-02-15 16:10:13 +0700

    gabungan

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,251 +1,165 @@
-import React from "react";
-import { StyleSheet, Image, Text, View, TouchableOpacity } from "react-native"; 
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from "react-native";
+const Background = require("../../assets/background.jpg");
+const image = require("../../assets/gabungan.png");
 
-const OptionScreen = ({navigation}) => {
-  const handleBuatAkunPress = () => {
-    navigation.navigate("Daftar"); // Navigate to the 'Daftar' screen
+export default function Daftar() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Fungsi untuk menangani login
+  const handleLogin = () => {
+    // Logika autentikasi bisa ditambahkan di sini
+    // Misalnya, memeriksa username dan password dengan data di server
+    console.log("Username:", username);
+    console.log("Password:", password);
+    // ... logika autentikasi lainnya
   };
 
   return (
-    <View style={styles.OptionScreen}>
-      <Image
-        style={styles.IphoneX11ProLightBase}
-        source={{
-          uri: "https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/dc5r7sxjopr-2136%3A307?alt=media&token=82a47207-79c3-4828-b3af-8e99aa1c774b",
-        }}
-      />
-      <Image
-        style={styles.Group9}
-        source={{
-          uri: "https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/dc5r7sxjopr-2118%3A7827?alt=media&token=4da9a752-41f2-44e5-9d16-c563f5578a58",
-        }}
-      />
-      <Image
-        style={styles.O}
-        source={{
-          uri: "https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/dc5r7sxjopr-2136%3A300?alt=media&token=c63c7dc1-a59f-42b4-b72c-2a419841a072",
-        }}
-      />
-      <Image
-        style={styles.Frame4234}
-        source={{
-          uri: "https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/dc5r7sxjopr-2118%3A8299?alt=media&token=68864d1b-991c-4eb0-8708-7265e7e73e11",
-        }}
-      />
-      <View style={styles.Frame4238}>
-        <View style={styles.LoginOrSignUp}>
-          <Text style={styles.MasukAtauDaftar}>Masuk atau Daftar</Text>
-          <Text style={styles.SilahkanMasukAtauDaf}>
-            Silahkan masuk atau daftar untuk menikmati permainan
-          </Text>
-        </View>
-        <View style={styles.Frame4232}>
-          <View style={styles.TextField}>
-            <Text style={styles.Text}>Username</Text>
-          </View>
-          <View style={styles.TextField1}>
-            <Text style={styles.Text2}>Password</Text>
-          </View>
-          <View style={styles.Buttons}>
-            <View style={styles.Button1}>
-              <Text style={styles.Button}>Login</Text>
-            </View>
-            <TouchableOpacity onPress={handleBuatAkunPress}>
-              <Text style={styles.BuatAkunBaru}>Buat akun baru</Text>
-            </TouchableOpacity>
-          </View>
+    <ImageBackground
+      source={Background}
+      resizeMode="cover"
+      style={styles.background}
+    >
+      <View>
+        <Image source={image} style={styles.gambar1} />
+      </View>
+      <View style={styles.card}>
+        <Text
+          style={{
+            textAlign: "center",
+            fontSize: 30,
+            fontFamily: "Rubik",
+            fontWeight: "bold",
+            margin: 10,
+            marginTop: 30,
+          }}
+        >
+          Masuk atau Daftar
+        </Text>
+        <Text style={{ textAlign: "center", fontSize: 20, color: "#858494" }}>
+          Silahkan daftar untuk menikmati permainan
+        </Text>
+
+        <View style={styles.container}>
+          <TextInput
+            style={styles.input}
+            placeholder="Username"
+            onChangeText={(text) => setUsername(text)}
+            value={username}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Password"
+            secureTextEntry={true}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+
+          <TouchableOpacity style={styles.button} onPress={handleLogin}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button1} onPress={handleLogin}>
+            <Text style={styles.buttonText1}>Buat Akun Baru</Text>
+          </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  OptionScreen: {
-    position: "relative",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    width: 375,
-    height: 812,
-    paddingBottom: 32,
-    boxSizing: "border-box",
-    backgroundColor: "rgba(106,90,224,1)",
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 50,
+    marginBottom: 30,
   },
-  IphoneX11ProLightBase: {
+  background: {
+    flex: 1,
     width: "100%",
-    height: 44,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  Group9: {
-    position: "absolute",
-    top: 73,
-    left: 18,
-    width: 338.24,
-    height: 271.24,
+  gambar1: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 70,
   },
-  O: {
-    position: "absolute",
-    top: 100,
-    left: -185,
-    width: 636,
-    height: 551,
-  },
-  Frame4234: {
-    position: "absolute",
-    top: 73,
-    left: 18,
-    width: 338.24,
-    height: 271.24,
-  },
-  Frame4238: {
-    position: "absolute",
-    top: 382,
-    left: 16,
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
+  card: {
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    backgroundColor: "#fff",
+    marginBottom: 70,
+    borderRadius: 40,
     width: 343,
-    height: 398,
-    paddingLeft: 16,
-    paddingRight: 16,
-    paddingTop: 16,
-    paddingBottom: 34,
-    borderRadius: 20,
-    boxSizing: "border-box",
-    backgroundColor: "rgba(255,255,255,1)",
+    height: 420,
   },
-  LoginOrSignUp: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    paddingLeft: 30,
-    paddingRight: 30,
-    boxSizing: "border-box",
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
   },
-  MasukAtauDaftar: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    color: "rgba(0,24,51,1)",
-    fontSize: 24, // Removed quotes around the value
-    lineHeight: "150%",
-    fontFamily: "Rubik, sans-serif",
-    fontWeight: "500",
-    textAlign: "center",
-  },
-  SilahkanMasukAtauDaf: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    color: "rgba(133,132,148,1)",
-    fontSize: 16, // Removed quotes around the value
-    lineHeight: "150%",
-    fontFamily: "Rubik, sans-serif",
-    fontWeight: "400",
-    textAlign: "center",
-  },
-  Frame4232: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
-    width: "100%",
-    boxSizing: "border-box",
-  },
-  TextField: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    width: "100%",
-    marginRight: 16,
-    paddingLeft: 15,
-    paddingRight: 15,
-    paddingTop: 11,
-    paddingBottom: 11,
+  input: {
+    height: 40,
+    width: "80%",
+    borderColor: "gray",
     borderWidth: 1,
-    borderColor: "rgba(226,232,240,1)",
-    borderRadius: 8,
-    boxSizing: "border-box",
-    backgroundColor: "rgba(255,255,255,1)",
+    marginBottom: 16,
+    padding: 8,
   },
-  Text: {
-    color: "rgba(100,116,139,1)",
-    fontSize: 16, // Removed quotes around the value
-    lineHeight: "150%",
-    fontFamily: "Inter, sans-serif",
-    fontWeight: "400",
-  },
-  TextField1: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "flex-start",
+
+  button: {
+    backgroundColor: "#6A5AE0",
+    padding: 12,
+    margin: 20,
+    borderRadius: 24,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    width: 311,
     alignItems: "center",
-    width: "100%",
-    marginRight: 16,
-    paddingLeft: 15,
-    paddingRight: 15,
-    paddingTop: 11,
-    paddingBottom: 11,
-    borderWidth: 1,
-    borderColor: "rgba(226,232,240,1)",
-    borderRadius: 8,
-    boxSizing: "border-box",
-    backgroundColor: "rgba(255,255,255,1)",
   },
-  Text2: {
-    color: "rgba(100,116,139,1)",
-    fontSize: 16, // Removed quotes around the value
-    lineHeight: "150%",
-    fontFamily: "Inter, sans-serif",
-    fontWeight: "400",
-  },
-  Buttons: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    width: "100%",
-    boxSizing: "border-box",
-  },
-  Button1: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    paddingLeft: 131,
-    paddingRight: 131,
-    paddingTop: 14,
-    paddingBottom: 14,
-    borderRadius: 20,
-    boxSizing: "border-box",
-    backgroundColor: "rgba(106,90,224,1)",
-  },
-  Button: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    color: "rgba(255,255,255,1)",
-    fontSize: 16, // Removed quotes around the value
-    lineHeight: "150%",
-    fontFamily: "Rubik, sans-serif",
+  buttonText: {
+    color: "white",
+    fontSize: 20,
     fontWeight: "500",
-    textAlign: "center",
+    paddingHorizontal: 12,
   },
-  BuatAkunBaru: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    color: "rgba(106,90,224,1)",
-    fontSize: 16, // Removed quotes around the value
-    lineHeight: "150%",
-    fontFamily: "Rubik, sans-serif",
+  buttonText1: {
+    color: "#6A5AE0",
+    fontSize: 16,
     fontWeight: "500",
-    textAlign: "center",
+  },
+  button1: {
+    padding: 12,
+    margin: 10,
+    borderRadius: 24,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.26,
+    width: 311,
+    alignItems: "center",
   },
 });
-
-export default OptionScreen;
