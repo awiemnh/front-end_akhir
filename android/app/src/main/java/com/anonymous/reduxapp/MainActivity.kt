0dead001b81d1a4e93642041f8bc0de5f5f11e61
yudharp <yudhaananda.rp@gmail.com>
2024-02-18 10:46:34 +0700

    reinstall android build

--- android/app/src/main/java/com/anonymous/reduxapp/MainActivity.kt
+++ android/app/src/main/java/com/anonymous/reduxapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.yudharp.projectakhir
+package com.anonymous.reduxapp
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/anonymous/reduxapp/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/reduxapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.yudharp.projectakhir
+package com.anonymous.reduxapp
 
 import android.app.Application
 import android.content.res.Configuration
